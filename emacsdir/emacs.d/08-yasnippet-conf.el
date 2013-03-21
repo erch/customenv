69d6584025e3bfb9a7c0cd5c4659d1362670a7cb
Eric Chastan <eric@chastan-jeannin.fr>
2013-03-21 17:41:24 +0100

    synch up from Chamrousse

--- emacsdir/emacs.d/08-yasnippet-conf.el
+++ emacsdir/emacs.d/08-yasnippet-conf.el
@@ -1,5 +1,6 @@
 (eval-and-compile 
   ;(add-to-list 'load-path (expand-file-name "yasnippet"  site-lisp-dir))
-  (require 'yasnippet) ;; not yasnippet-bundle
-  (yas/initialize))
+  (when (not (require 'yasnippet nil t))
+    (package-install 'yasnippet-bundle))
+   (yas/initialize)) 
   ;(yas/load-directory (expand-file-name "yasnippet/snippets"  site-lisp-dir)))
