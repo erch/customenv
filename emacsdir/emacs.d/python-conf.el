8e654eb951b6b3c35f85a806e162bc0603e92fc5
Eric Chastan <echastan@bestofmedia.com>
2013-03-21 17:42:49 +0100

    from lamet

--- emacsdir/emacs.d/python-conf.el
+++ emacsdir/emacs.d/python-conf.el
@@ -1,3 +1,26 @@
 (custom-set-variables
  '(python-guess-indent nil)
  '(python-indent 4))
+
+(require 'python)
+(require 'auto-complete)
+(require 'yasnippet)
+(setq py-load-pymacs-p nil)
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+;; Initialize Pymacs                                                                                          
+(setq pymacs-python-command  "/usr/bin/python2.7") 
+
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
+
+;; Initialize Rope         
+(pymacs-load "ropemacs" "rope-")                                                                                    
+(setq ropemacs-enable-autoimport t)
+
--- shell/profile.d/python.sh
+++ shell/profile.d/python.sh
@@ -1,3 +1,5 @@
- 
-echo "python script"
 pathmunge  ${HOME}/.local/bin after
+export PYTHONPATH="/home/ech/.local/lib/python2.7/site-packages/:/usr/local/lib/python2.7/dist-packages/:/usr/lib/python2.7:/usr/lib/python2.7/plat-linux2:/usr/lib/python2.7/lib-tk:/usr/lib/python2.7/lib-old:/usr/lib/python2.7/lib-dynload:/usr/local/lib/python2.7/dist-packages"
+
+
+
