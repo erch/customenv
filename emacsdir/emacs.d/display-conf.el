083f630465f7e53ed0b1d5f3de979b6dfba158e7
Eric Chastan <echastan@bestofmedia.com>
2013-04-15 10:19:05 +0200

    come back to term instead of multi-term

--- emacsdir/emacs.d/display-conf.el
+++ emacsdir/emacs.d/display-conf.el
@@ -28,7 +28,111 @@
     (package-install 'framemove)
     (require 'framemove)
 ))
+
 (framemove-default-keybindings)
 (windmove-default-keybindings)
 (setq framemove-hook-into-windmove t)
+
+(unless (require 'frame-fns nil t)
+  (progn
+    (package-install 'frame-fns)
+    (require 'frame-fns)
+))
+
+(unless (require 'frame-cmds nil t)
+  (progn
+    (package-install 'frame-cmds)
+    (require 'frame-cmds)
+))
+
+;;   (global-set-key [(meta up)]                    'move-frame-up)
+;;   (global-set-key [(meta down)]                  'move-frame-down)
+;;   (global-set-key [(meta left)]                  'move-frame-left)
+;;   (global-set-key [(meta right)]                 'move-frame-right)
+(global-set-key [(meta shift ?v)]              'move-frame-to-screen-top)      ; like `M-v'
+(global-set-key [(control shift ?v)]           'move-frame-to-screen-bottom)   ; like `C-v'
+(global-set-key [(control shift prior)]        'move-frame-to-screen-left)     ; like `C-prior'
+(global-set-key [(control shift next)]         'move-frame-to-screen-right)    ; like `C-next'
+(global-set-key [(control meta down)]          'enlarge-frame)
+(global-set-key [(control meta right)]         'enlarge-frame-horizontally)
+(global-set-key [(control meta up)]            'shrink-frame)
+(global-set-key [(control meta left)]          'shrink-frame-horizontally)
+;;   (global-set-key [(control ?x) (control ?z)]    'iconify-everything)
+;;   (global-set-key [vertical-line S-down-mouse-1] 'iconify-everything)
+;;   (global-set-key [(control ?z)]                 'iconify/map-frame)
+;;   (global-set-key [mode-line mouse-3]            'mouse-iconify/map-frame)
+;;   (global-set-key [mode-line C-mouse-3]          'mouse-remove-window)
+;;   (global-set-key [(control meta ?z)]            'show-hide)
+;;   (global-set-key [vertical-line C-down-mouse-1] 'show-hide)
+;;   (global-set-key [C-down-mouse-1]               'mouse-show-hide-mark-unmark)
+;;   (substitute-key-definition 'delete-window      'remove-window global-map)
+;;   (define-key ctl-x-map "o"                      'other-window-or-frame)
+;;   (define-key ctl-x-4-map "1"                    'delete-other-frames)
+;;   (define-key ctl-x-5-map "h"                    'show-*Help*-buffer)
+;;   (substitute-key-definition 'delete-window      'delete-windows-for global-map)
+;;   (define-key global-map "\C-xt."                'save-frame-config)
+;;   (define-key ctl-x-map "o"                      'other-window-or-frame)
+;;
+;;   (defalias 'doremi-prefix (make-sparse-keymap))
+;;   (defvar doremi-map (symbol-function 'doremi-prefix) "Keymap for Do Re Mi commands.")
+;;   (define-key global-map "\C-xt" 'doremi-prefix)
+;;   (define-key doremi-map "." 'save-frame-config)
+;;
+;;  Customize the menu.  Uncomment this to try it out.
+;;
+;;
+(defvar menu-bar-frames-menu (make-sparse-keymap "Frames"))
+(define-key global-map [menu-bar frames]
+  (cons "Frames" menu-bar-frames-menu))
+;)
+(define-key menu-bar-frames-menu [set-all-params-from-frame]
+  '(menu-item "Set All Frame Parameters from Frame" set-all-frame-alist-parameters-from-frame
+    :help "Set frame parameters of a frame to their current values in frame"))
+(define-key menu-bar-frames-menu [set-params-from-frame]
+  '(menu-item "Set Frame Parameter from Frame..." set-frame-alist-parameter-from-frame
+    :help "Set parameter of a frame alist to its current value in frame"))
+(define-key menu-bar-frames-menu [separator-frame-1] '("--"))
+(define-key menu-bar-frames-menu [tile-frames-vertically]
+  '(menu-item "Tile Frames Vertically..." tile-frames-vertically
+    :help "Tile all visible frames vertically"))
+(define-key menu-bar-frames-menu [tile-frames-horizontally]
+  '(menu-item "Tile Frames Horizontally..." tile-frames-horizontally
+    :help "Tile all visible frames horizontally"))
+(define-key menu-bar-frames-menu [separator-frame-2] '("--"))
+(define-key menu-bar-frames-menu [toggle-max-frame-vertically]
+  '(menu-item "Toggle Max Frame Vertically" toggle-max-frame-vertically
+    :help "Maximize or restore the selected frame vertically"
+    :enable (frame-parameter nil 'restore-height)))
+(define-key menu-bar-frames-menu [toggle-max-frame-horizontally]
+  '(menu-item "Toggle Max Frame Horizontally" toggle-max-frame-horizontally
+    :help "Maximize or restore the selected frame horizontally"
+    :enable (frame-parameter nil 'restore-width)))
+(define-key menu-bar-frames-menu [toggle-max-frame]
+  '(menu-item "Toggle Max Frame" toggle-max-frame
+    :help "Maximize or restore the selected frame (in both directions)"
+    :enable (or (frame-parameter nil 'restore-width) (frame-parameter nil 'restore-height))))
+(define-key menu-bar-frames-menu [maximize-frame-vertically]
+  '(menu-item "Maximize Frame Vertically" maximize-frame-vertically
+    :help "Maximize the selected frame vertically"))
+(define-key menu-bar-frames-menu [maximize-frame-horizontally]
+  '(menu-item "Maximize Frame Horizontally" maximize-frame-horizontally
+    :help "Maximize the selected frame horizontally"))
+(define-key menu-bar-frames-menu [maximize-frame]
+  '(menu-item "Maximize Frame" maximize-frame
+    :help "Maximize the selected frame (in both directions)"))
+(define-key menu-bar-frames-menu [separator-frame-3] '("--"))
+(define-key menu-bar-frames-menu [iconify-everything]
+  '(menu-item "Iconify All Frames" iconify-everything
+    :help "Iconify all frames of session at once"))
+(define-key menu-bar-frames-menu [show-hide]
+  '(menu-item "Hide Frames / Show Buffers" show-hide
+    :help "Show, if only one frame visible; else hide."))
+;)
+
+;; (defvar menu-bar-doremi-menu (make-sparse-keymap "Do Re Mi"))
+;; (define-key global-map [menu-bar doremi]
+;;   (cons "Do Re Mi" menu-bar-doremi-menu))
+;; (define-key menu-bar-doremi-menu [doremi-font+]
+;;   '("Save Frame Configuration" . save-frame-config))
+
 (provide 'display-conf)
--- emacsdir/emacs.d/shell-conf.el
+++ emacsdir/emacs.d/shell-conf.el
@@ -1,29 +1,92 @@
 (message "loading shell-conf ...")
-(unless (require 'multi-term nil t)
-  (progn
-    (package-install 'multi-term)
-    (require 'multi-term)))
+(require 'term)
 
-(multi-term-keystroke-setup) 
-(global-set-key (kbd "<f2>") 'multi-term)
-(global-set-key (kbd "S-<f2>") 'multi-term-next)
+;(unless (require 'multi-term nil t)
+;  (progn
+;    (package-install 'multi-term)
+;    (require 'multi-term)))
 
+;(multi-term-keystroke-setup) 
+;(global-set-key (kbd "<f2>") 'ansi-term)
+;(global-set-key (kbd "S-<f2>") 'multi-term-next)
+
+;(setq explicit-shell-file-name "/bin/bash")
+
+(defun visit-ansi-term ()
+  "If the current buffer is:
+1) a running ansi-term named *ansi-term*, rename it.
+2) a stopped ansi-term, kill it and create a new one.
+3) a non ansi-term, go to an already running ansi-term
+or start a new one while killing a defunt one"
+  (interactive)
+  (let ((is-term (string= "term-mode" major-mode))
+        (is-running (term-check-proc (buffer-name)))
+        (term-cmd (if (boundp 'explicit-shell-file-name) explicit-shell-file-name nil))
+        (anon-term (get-buffer "*ansi-term*")))
+    (if is-term
+        (if is-running
+            (if (string= "*ansi-term*" (buffer-name))
+                (call-interactively 'rename-buffer)
+              (if anon-term
+                  (switch-to-buffer "*ansi-term*")
+                (ansi-term term-cmd)))
+          (kill-buffer (buffer-name))
+          (ansi-term term-cmd))
+      (if anon-term
+          (if (term-check-proc "*ansi-term*")
+              (switch-to-buffer "*ansi-term*")
+            (kill-buffer "*ansi-term*")
+            (ansi-term term-cmd))
+        (ansi-term term-cmd)))))
+
+; (get-buffer "*ansi-term*")
+(global-set-key (kbd "<f2>") 'visit-ansi-term)
+
+;(setq term-mode-hook ())
 (add-hook 'term-mode-hook
-	  '(lambda ()
+	  '(lambda ()	     
 	     (setq-local overflow-newline-into-fringe t)
-	     (define-key term-raw-map (kbd "C-c C-j") 'term-line-mode)
-	     (setq-local  multi-term-scroll-show-maximum-output t)
-	     (setq-local  multi-term-scroll-to-bottom-on-output "all")
+	     ;(define-key term-raw-map (kbd "C-c C-j") 'term-line-mode)
 	     (setq-local  autopair-dont-activate t)
-	     (yas-minor-mode -1)))
+	     (yas-minor-mode -1)
+	     (ansi-color-for-comint-mode-on) 
+	     (toggle-truncate-lines 1)
+	     (setq-local comint-scroll-to-bottom-on-input t)  ; always insert at the bottom
+	     (setq-local comint-scroll-to-bottom-on-output nil) ; always add output at the bottom
+	     (setq-local comint-scroll-show-maximum-output t) ; scroll to show max possible output
+	     (setq-local  comint-completion-autolist t)     ; show completion list when ambiguous
+	     (setq-local comint-input-ignoredups t)           ; no duplicates in command history
+	     (setq-local comint-completion-addsuffix t)       ; insert space/slash after file completion
+	     (setq-local comint-buffer-maximum-size 50000)    ; max length of the buffer in lines
+	     (setq-local comint-prompt-read-only nil)         ; if this is t, it breaks shell-command
+	     (setq-local comint-get-old-input (lambda () "")) ; what to run when i press enter on a
+	     (setq-local comint-input-ring-size 5000)         ; max shell history size
+	     (setq-local  mouse-yank-at-point t)
+	     (setq-local transient-mark-mode nil)
+	     (auto-fill-mode -1)
+	     (setq-local tab-width 8 )
+))
+
+;; truncate buffers continuously
+(add-hook 'comint-output-filter-functions 'comint-truncate-buffer)
+
+; interpret and use ansi color codes in shell output windows
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+(defun set-scroll-conservatively ()
+  "Add to shell-mode-hook to prevent jump-scrolling on newlines in shell buffers."
+  (set (make-local-variable 'scroll-conservatively) 10))
+(add-hook 'shell-mode-hook 'set-scroll-conservatively)
 
 (unless (string= window-system "w32")
+  (setenv "PAGER" "cat")
+  (setq-local explicit-shell-file-name "/bin/bash")
   (add-hook 'term-mode-hook 
 	    '(lambda ()
 	        (setq-local comint-use-prompt-regexp t)
-		(setq-local shell-prompt-pattern "^[^$]+$ ")
-		(setq-local term-prompt-regexp "^[^$]+$ ")
-		(setq-local comint-prompt-regexp shell-prompt-pattern))))
+		(setq-local shell-prompt-pattern "^[^$#\n]*[$#] ")
+		(setq-local term-prompt-regexp "^[^$#>\n]*[$#>] *")
+		(setq-local comint-prompt-regexp "^[^$#>\n]*[$#>] *"))))
 			       
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initial setup
