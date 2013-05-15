493d933ae2ca2d97546dc6de19f302a8481fc059
Eric Chastan <eric@chastan-jeannin.fr>
2013-05-15 09:10:31 +0200

    synch from chamrousse

--- emacsdir/emacs.d/org-conf.el
+++ emacsdir/emacs.d/org-conf.el
@@ -44,7 +44,7 @@
 (setq org-enforce-todo-dependencies t)
 (setq org-treat-S-cursor-todo-selection-as-state-change nil)
 (setq org-agenda-prefix-format "%t %s")
-
+(setq odd-weeks '(1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49 51))
 ;; Managing org files
 (setq org-directory (file-name-as-directory (expand-file-name "Orga" my-home-dir)))
 
