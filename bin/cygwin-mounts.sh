3b4eddaa99907c771a67daec319f11eb8551bf7e
ech <eric@chastan-jeannin.fr>
2015-01-15 21:25:19 +0100

    added support for vagrant

--- bin/cygwin-mounts.sh
+++ bin/cygwin-mounts.sh
@@ -1,13 +1,14 @@
+VM_HOMES=/cygdrive/c/homes/
+CYG_HOME=${VM_HOMES}/cyghome/${USER}
+mkdir -p $CYG_HOME
 echo "C:  /c  ntfs binary,nouser 0 0"  >> /etc/fstab
-echo "`cygpath -wa $HOMEPATH/..`  /home ntfs binary,nouser 0 0"  >> /etc/fstab
-if [ ! -e /var/local ] ; then mkdir -p /var/local ; fi
-echo "`cygpath -wa $VAR_LOCAL`  /var/local ntfs binary,nouser 0 0"   >> /etc/fstab
-if [ ! -e /opt/local ] ; then mkdir -p /opt/local ; fi
-echo "`cygpath -wa $OPT_LOCAL`  /opt/local ntfs binary,nouser 0 0"  >> /etc/fstab
-echo "`cygpath -wa $VAR_LOCAL/tmp`  /tmp ntfs binary,nouser 0 0"  >> /etc/fstab
-if [ ! -e $HOME/bin ] ; then mkdir -p $HOMEPATH/bin ; fi
-echo "`cygpath -wa $DEV_HOME/shell/bin`  $HOME/bin ntfs binary,nouser 0 0"  >> /etc/fstab
-if [ ! -e $HOME/.profile.d ] ; then mkdir -p $HOMEPATH/.profile.d ; fi
-echo "`cygpath -wa $DEV_HOME/shell/profile.d`  $HOME/.profile.d ntfs binary,nouser 0 0"  >> /etc/fstab
-if [ ! -e /var/www ] ; then mkdir -p /var/www ; fi
-echo "`cygpath -wa $WEB_HOME`  /var/www ntfs binary,nouser 0 0"  >> /etc/fstab
+echo "$(cygpath -wm $HOME) /home ntfs binary,nouser 0 0"  >> /etc/fstab
+mount -a
+setx HOME $(cygpath -wa $HOME)
+setx VM_HOMES $(cygpath -wa $VM_HOMES)
+
+# Vagrant file
+VAGRANT_HOMES=${VM_HOMES}/vagrant
+mkdir -p $VAGRANT_HOMES
+
+setx MY_HOME $(cygpath -wa '/c/MyHome)
