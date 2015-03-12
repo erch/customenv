ebb2692d40a8241cc1e91740c4eab94152487a02
U-CRITEOIS\e.chastan <e.chastan@FR-F6S0342.criteois.lan>
2015-03-12 14:37:59 +0100

    attempt to manage line ending

--- packages/setup-echenv/bin/pre-inst/cygwin/init-cygwin.sh
+++ packages/setup-echenv/bin/pre-inst/cygwin/init-cygwin.sh
@@ -7,7 +7,5 @@ export MY_ENV=/cygdrive/c/MyEnv
 /cygdrive/c/Windows/system32/setx MY_ENV $(cygpath -wa "$MY_ENV")
 export WIN_OPT=/cygdrive/c/WinOpt
 /cygdrive/c/Windows/system32/setx WIN_OPT $(cygpath -wa "$WIN_OPT")
-export EMACS_HOME=${WIN_OPT}/Emacs/bin
-/cygdrive/c/Windows/system32/setx EMACS_HOME $(cygpath -wa "$EMACS_HOME")
 /cygdrive/c/Windows/system32/setx CYGWIN_ROOT $(cygpath -wa "/")
 /cygdrive/c/Windows/system32/setx HOME $(cygpath -wa ~)
